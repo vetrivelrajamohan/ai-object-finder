fa267172390f3cb608b03055bed437a27ebab3d8
vetrivelrajamohan <91267642+vetrivelrajamohan@users.noreply.github.com>
2022-07-28 22:06:52 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,24 @@
+objects = [];
+status = "";
+
+function setup()
+{
+    canvas = createCanvas(480, 380);
+    canvas.center();
+    video = createCapture(VIDEO);
+    video.size(480, 380);
+    video.hide();
+}
+
+function start()
+{
+    objectDetector = ml5.objectDetector('cocossd', modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+    object_name = document.getElementById("object_name").value;
+}
+
+function modelLoaded()
+{
+    console.log("Model Loaded!")
+    status = true;
+}
